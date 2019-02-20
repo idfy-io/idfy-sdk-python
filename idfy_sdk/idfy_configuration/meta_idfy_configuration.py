3434f60ed507ed73920bbcd4a6535eccb8b6e90b
EivindSB <Eivind@idfy.io>
2019-02-20 09:42:17 +0100

    Try to avoid pre-3.7 import bug v2

--- idfy_sdk/idfy_configuration/meta_idfy_configuration.py
+++ idfy_sdk/idfy_configuration/meta_idfy_configuration.py
@@ -1,5 +1,5 @@
 from idfy_sdk import urls as urls
-from idfy_sdk.version import version
+from idfy_sdk import version as ver
 
 class MetaIdfyConfiguration(type):
 
@@ -29,7 +29,7 @@ class MetaIdfyConfiguration(type):
     
     @property
     def SdkVersion(self):
-        return version
+        return ver
 
     @property
     def ClientId(self):
